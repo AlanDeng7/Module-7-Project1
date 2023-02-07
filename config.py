12167ea4b775585d5098bb5a9d4c72231417990d
AlanDeng7 <alan.rb.deng@gmail.com>
2023-02-07 14:20:32 -0500

    categories added

---
+++ config.py
@@ -0,0 +1,6 @@
+#Yelp Fusion API Key
+api_key = "iK5hfZ_62q1A-n-HuyAtkst69DRIouHvAvcDgxlfrkPCe5v809dBN7eI6AOsk6_jURZANYG-gc6yZ67egnTjLav2ML7hTPD7e1ph7F5L_TULJ0-ZjjPyLQyJXIjhY3Yx"
+
+client_id = "8XoX5Q1eKxWYyr9GSIo9MA"
+
+
